5a3cabf941c7238d43cc5a546ae69ad229f607e8
Andrey Cherkashin <action.open@gmail.com>
2020-11-05 18:54:01 +0100

    + extensions

---
+++ Utils/TimeUtils.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Plugins.UnityMonstackCore.Utils
+{
+    public static class TimeUtils
+    {
+        public static uint Unixtime => (uint) (DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1))).TotalSeconds;
+    }
+}
