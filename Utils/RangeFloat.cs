a65b4ef830e55d0811dab24aebbaa9e3b77d1637
Iurii <yuriy@realwelders.com>
2021-01-09 19:47:48 +0200

    - implemented range struct

---
+++ Utils/RangeFloat.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+
+namespace Plugins.Shared.UnityMonstackCore.Utils
+{
+    [Serializable]
+    public struct RangeFloat
+    {
+        public float min;
+        public float max;
+
+        public float Get(float t)
+        {
+            return Mathf.Lerp(min, max, t);
+        }
+    }
+}
---
+++ Utils/RangeInt.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+
+namespace Plugins.Shared.UnityMonstackCore.Utils
+{
+    [Serializable]
+    public struct RangeInt
+    {
+        public int min;
+        public int max;
+
+        public int Get(float t)
+        {
+            return (int)Mathf.Lerp(min, max, t);
+        }
+    }
+}
