3a73798df108f7a8e1e028f246b7860ee5e9d118
Andrey Cherkashin <action.open@gmail.com>
2021-01-27 19:32:50 +0100

    + adding vector extensions

--- Extensions/VectorExtensions.cs
+++ Extensions/VectorExtensions.cs
@@ -15,6 +15,11 @@ namespace Plugins.UnityMonstackCore.Extensions
             return new float2(v.x, v.z);
         }
 
+        public static int2 ToInt2(this Vector2 v)
+        {
+            return new int2((int) math.floor(v.x), (int) math.floor(v.y));
+        }
+
         public static int3 ToInt3(this Vector3 v)
         {
             return new int3((int) math.floor(v.x), (int) math.floor(v.y), (int) math.floor(v.z));
@@ -28,6 +33,11 @@ namespace Plugins.UnityMonstackCore.Extensions
         public static float3 ToFloat3(this int2 v, float y = 0)
         {
             return new float3(v.x, y, v.y);
+        }
+
+        public static float2 ToFloat2(this int2 v)
+        {
+            return new float2(v.x,  v.y);
         }
 
         public static float3 ToFloat3(this float2 v, float y = 0)
@@ -47,7 +57,7 @@ namespace Plugins.UnityMonstackCore.Extensions
 
         public static int2 ToInt2(this float2 v)
         {
-            return new int2((int) v.x, (int) v.y);
+            return new int2((int) math.floor(v.x), (int) math.floor(v.y));
         }
 
         public static int2 ToInt2(this float3 v)
