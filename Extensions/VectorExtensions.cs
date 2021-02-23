e2a4705ec7ec790cd745e0df5123c20298604d3b
Andrey Cherkashin <action.open@gmail.com>
2021-02-23 21:37:40 +0100

    + more extension methods

--- Extensions/VectorExtensions.cs
+++ Extensions/VectorExtensions.cs
@@ -14,6 +14,16 @@ namespace Plugins.UnityMonstackCore.Extensions
         {
             return new float2(v.x, v.z);
         }
+
+        public static Vector2 ToVector2(this Vector3 v)
+        {
+            return new Vector2(v.x, v.z);
+        }
+
+        public static Vector3 ToVector3(this Vector2 v)
+        {
+            return new Vector3(v.x, 0, v.y);
+        }
         
         public static float3 ToFloat3(this Vector3 v)
         {
