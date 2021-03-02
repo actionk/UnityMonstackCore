6230ac5382b5fe9a81ff836aecd493deb5172dcc
Andrey Cherkashin <action.open@gmail.com>
2021-03-02 22:41:20 +0100

    + some fixes

--- Utils/RangeInt.cs
+++ Utils/RangeInt.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Sirenix.OdinInspector;
 using UnityEngine;
 
 namespace Plugins.Shared.UnityMonstackCore.Utils
@@ -6,12 +7,15 @@ namespace Plugins.Shared.UnityMonstackCore.Utils
     [Serializable]
     public struct RangeInt
     {
+        [HorizontalGroup("Range"), LabelWidth(100)]
         public int min;
+
+        [HorizontalGroup("Range"), LabelWidth(100)]
         public int max;
 
         public int Get(float t)
         {
-            return (int)Mathf.Lerp(min, max, t);
+            return (int) Mathf.Lerp(min, max, t);
         }
     }
 }
