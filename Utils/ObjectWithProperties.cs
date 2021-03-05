e2d66de7b28f6572150f0027a07126553423bafe
Andrey Cherkashin <action.open@gmail.com>
2021-03-05 19:49:56 +0100

    + adding object with properties

---
+++ Utils/ObjectWithProperties.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Plugins.Shared.UnityMonstackCore.Utils
+{
+    public class ObjectWithProperties
+    {
+        private readonly Dictionary<Type, object> m_properties = new Dictionary<Type, object>();
+
+        public void AddProperty<T>(T property)
+        {
+            SetProperty(property);
+        }
+
+        public void SetProperty<T>(T property)
+        {
+            m_properties[typeof(T)] = property;
+        }
+
+        public bool RemoveProperty<T>()
+        {
+            return m_properties.Remove(typeof(T));
+        }
+
+        public T GetProperty<T>()
+        {
+            return (T) m_properties[typeof(T)];
+        }
+
+        public bool HasProperty<T>()
+        {
+            return HasProperty(typeof(T));
+        }
+
+        public bool HasProperty(Type type)
+        {
+            return m_properties.ContainsKey(type);
+        }
+
+        public bool TryGetProperty<T>(out T property)
+        {
+            bool hasProperty = m_properties.TryGetValue(typeof(T), out object objectProperty);
+            property = (T) objectProperty;
+            return hasProperty;
+        }
+
+        public T GetOrCreateProperty<T>(Func<T> createCallback)
+        {
+            var key = typeof(T);
+            if (m_properties.TryGetValue(key, out object value))
+                return (T) value;
+
+            var newValue = createCallback.Invoke();
+            m_properties[key] = newValue;
+            return newValue;
+        }
+
+        public T GetOrCreateProperty<T>() where T : new()
+        {
+            var key = typeof(T);
+            if (m_properties.TryGetValue(key, out object value))
+                return (T) value;
+
+            var newValue = new T();
+            m_properties[key] = newValue;
+            return newValue;
+        }
+    }
+}
