7b27ebcdd4027550be19c31747b25affd8894b05
Andrey Cherkashin <action.open@gmail.com>
2021-03-05 15:46:09 +0100

    + adding bounds extensions

---
+++ Extensions/BoundsExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Unity.Mathematics;
+using UnityEngine;
+
+namespace Plugins.UnityMonstackCore.Extensions
+{
+    public static class BoundsExtensions
+    {
+        public static float2x2 ToFloat2x2(this Bounds bounds)
+        {
+            return new float2x2(
+                new float2(bounds.min.x, bounds.min.z),
+                new float2(bounds.size.x, bounds.size.z)
+            );
+        }
+    }
+}
