2340882de1ff36e34ec6dfd045cce337724a7e02
Andrey Cherkashin <action.open@gmail.com>
2021-05-21 14:27:28 +0200

    + fixing random reactions

---
+++ Extensions/RandomExtensionTest.cs
@@ -0,0 +1,32 @@
+﻿using Plugins.UnityMonstackCore.Extensions;
+using Sirenix.OdinInspector;
+using UnityEngine;
+using Random = System.Random;
+
+namespace Plugins.Shared.UnityMonstackCore.Extensions
+{
+    public class RandomExtensionTest : MonoBehaviour
+    {
+        public GameObject prefab;
+        public int count;
+        public int seed;
+        public float radius;
+
+        [Button]
+        public void Clean()
+        {
+            transform.DestroyChildren(true);
+        }
+
+        [Button]
+        public void Random()
+        {
+            var random = new Random(seed);
+            for (var i = 0; i < count; i++)
+            {
+                var randomValue = random.Next2DPointInRadius(radius);
+                var created = Instantiate(prefab, randomValue.ToFloat3(), Quaternion.identity, transform);
+            }
+        }
+    }
+}
