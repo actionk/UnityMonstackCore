e420be5197d92436bd3d11c4c2ce9e902302b7b4
Andrey Cherkashin <action.open@gmail.com>
2021-05-26 19:33:53 +0200

    + adding datetime extensions

---
+++ Extensions/DateTimeExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Plugins.Shared.UnityMonstackCore.Extensions
+{
+    public static class DateTimeExtensions
+    {
+        public static DateTime ToDateTime(this ulong unixTimeStamp)
+        {
+            DateTime dtDateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+            dtDateTime = dtDateTime.AddSeconds(unixTimeStamp).ToLocalTime();
+            return dtDateTime;
+        }
+    }
+}
