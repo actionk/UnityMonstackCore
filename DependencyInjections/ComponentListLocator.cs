d882a9ad6636a866f1602f188d54901dd7ea0148
Andrey Cherkashin <action.open@gmail.com>
2021-06-08 15:37:09 +0200

    + new components locator

---
+++ DependencyInjections/ComponentListLocator.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Plugins.Shared.UnityMonstackCore.DependencyInjections
+{
+    public class ComponentListLocator<T> where T : Component
+    {
+        private List<T> m_instance;
+
+        public List<T> Components
+        {
+            get
+            {
+                if (m_instance == null)
+                    m_instance = Object.FindObjectsOfType<T>().ToList();
+
+                return m_instance;
+            }
+        }
+    }
+}
