18565a760d5dde49e44e1f222896c0413b228c96
Andrey Cherkashin <action.open@gmail.com>
2021-06-13 18:07:33 +0200

    + locator improvements

--- DependencyInjections/ComponentLocator.cs
+++ DependencyInjections/ComponentLocator.cs
@@ -6,15 +6,32 @@ namespace Plugins.Shared.UnityMonstackCore.DependencyInjections
     {
         private T m_instance;
 
+        public ComponentLocator(bool locateImmediately = false)
+        {
+            if (locateImmediately)
+                m_instance = ForceLocate();
+        }
+
         public T Component
         {
             get
             {
                 if (m_instance == null)
-                    m_instance = Object.FindObjectOfType<T>();
+                    m_instance = ForceLocate();
 
                 return m_instance;
             }
+        }
+
+        public void Locate()
+        {
+            if (m_instance == null)
+                m_instance = ForceLocate();
+        }
+
+        private static T ForceLocate()
+        {
+            return Object.FindObjectOfType<T>();
         }
     }
 }
