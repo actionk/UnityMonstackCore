ea847375fda937041c58066361b0518977cef4cc
abramov.mx.o <abramov.mx.o@gmail.com>
2021-07-15 20:13:12 +0200

    - finalizing death wave spell

--- Utils/OrderedMultiValueDictionary.cs
+++ Utils/OrderedMultiValueDictionary.cs
@@ -5,18 +5,18 @@ using System.Linq;
 namespace Plugins.Shared.UnityMonstackCore.Utils
 {
     /// <summary>
-    /// Extension to the normal Dictionary. This class can store more than one value for every key. It keeps a HashSet for every Key value.
+    /// Extension to the normal Dictionary. This class can store more than one value for every key. It keeps a List for every Key value.
     /// Calling Add with the same Key and multiple values will store each value under the same Key in the Dictionary. Obtaining the values
-    /// for a Key will return the HashSet with the Values of the Key. 
+    /// for a Key will return the List with the Values of the Key. 
     /// </summary>
     /// <typeparam name="TKey">The type of the key.</typeparam>
     /// <typeparam name="TValue">The type of the value.</typeparam>
-    public class MultiValueDictionary<TKey, TValue> : Dictionary<TKey, HashSet<TValue>>
+    public class OrderedMultiValueDictionary<TKey, TValue> : Dictionary<TKey, List<TValue>>
     {
         /// <summary>
-        /// Initializes a new instance of the <see cref="MultiValueDictionary&lt;TKey, TValue&gt;"/> class.
+        /// Initializes a new instance of the <see cref="OrderedMultiValueDictionary&lt;TKey, TValue&gt;"/> class.
         /// </summary>
-        public MultiValueDictionary()
+        public OrderedMultiValueDictionary()
             : base()
         {
         }
@@ -29,10 +29,10 @@ namespace Plugins.Shared.UnityMonstackCore.Utils
         /// <param name="value">The value.</param>
         public void Add(TKey key, TValue value)
         {
-            HashSet<TValue> container = null;
+            List<TValue> container = null;
             if (!this.TryGetValue(key, out container))
             {
-                container = new HashSet<TValue>();
+                container = new List<TValue>();
                 base.Add(key, container);
             }
 
@@ -45,12 +45,12 @@ namespace Plugins.Shared.UnityMonstackCore.Utils
         /// </summary>
         /// <param name="key">The key.</param>
         /// <param name="value">The value.</param>
-        public void AddRange(TKey key, HashSet<TValue> value)
+        public void AddRange(TKey key, List<TValue> value)
         {
-            HashSet<TValue> container = null;
+            List<TValue> container = null;
             if (!this.TryGetValue(key, out container))
             {
-                container = new HashSet<TValue>();
+                container = new List<TValue>();
                 base.Add(key, container);
             }
 
@@ -70,7 +70,7 @@ namespace Plugins.Shared.UnityMonstackCore.Utils
         public bool ContainsValue(TKey key, TValue value)
         {
             bool toReturn = false;
-            HashSet<TValue> values = null;
+            List<TValue> values = null;
             if (this.TryGetValue(key, out values))
             {
                 toReturn = values.Contains(value);
@@ -87,7 +87,7 @@ namespace Plugins.Shared.UnityMonstackCore.Utils
         /// <param name="value">The value.</param>
         public bool Remove(TKey key, TValue value)
         {
-            HashSet<TValue> container = null;
+            List<TValue> container = null;
             if (this.TryGetValue(key, out container))
             {
                 container.Remove(value);
@@ -125,17 +125,17 @@ namespace Plugins.Shared.UnityMonstackCore.Utils
 
 
         /// <summary>
-        /// Merges the specified multivaluedictionary into this instance.
+        /// Merges the specified OrderedMultiValueDictionary into this instance.
         /// </summary>
         /// <param name="toMergeWith">To merge with.</param>
-        public void Merge(MultiValueDictionary<TKey, TValue> toMergeWith)
+        public void Merge(OrderedMultiValueDictionary<TKey, TValue> toMergeWith)
         {
             if (toMergeWith == null)
             {
                 return;
             }
 
-            foreach (KeyValuePair<TKey, HashSet<TValue>> pair in toMergeWith)
+            foreach (KeyValuePair<TKey, List<TValue>> pair in toMergeWith)
             {
                 foreach (TValue value in pair.Value)
                 {
@@ -150,17 +150,17 @@ namespace Plugins.Shared.UnityMonstackCore.Utils
         /// (e.g. in lambdas)
         /// </summary>
         /// <param name="key">The key.</param>
-        /// <param name="returnEmptySet">if set to true and the key isn't found, an empty hashset is returned, otherwise, if the key isn't found, null is returned</param>
+        /// <param name="returnEmptySet">if set to true and the key isn't found, an empty List is returned, otherwise, if the key isn't found, null is returned</param>
         /// <returns>
         /// This method will return null (or an empty set if returnEmptySet is true) if the key wasn't found, or
         /// the values if key was found.
         /// </returns>
-        public HashSet<TValue> GetValues(TKey key, bool returnEmptySet = false)
+        public List<TValue> GetValues(TKey key, bool returnEmptySet = false)
         {
-            HashSet<TValue> toReturn = null;
+            List<TValue> toReturn = null;
             if (!base.TryGetValue(key, out toReturn) && returnEmptySet)
             {
-                toReturn = new HashSet<TValue>();
+                toReturn = new List<TValue>();
             }
 
             return toReturn;
@@ -168,21 +168,21 @@ namespace Plugins.Shared.UnityMonstackCore.Utils
 
         public IEnumerable<TValue> GetAllValues()
         {
-            foreach (var hashSet in Values)
+            foreach (var List in Values)
             {
-                foreach (var value in hashSet)
+                foreach (var value in List)
                 {
                     yield return value;
                 }
             }
         }
 
-        public HashSet<TValue> GetValues()
+        public List<TValue> GetValues()
         {
-            var result = new HashSet<TValue>();
-            foreach (var hashSet in Values)
+            var result = new List<TValue>();
+            foreach (var List in Values)
             {
-                foreach (var value in hashSet)
+                foreach (var value in List)
                     result.Add(value);
             }
 
@@ -190,38 +190,39 @@ namespace Plugins.Shared.UnityMonstackCore.Utils
         }
     }
 
-    public static class MultiValueDictionaryExtensions
+    public static class OrderedMultiValueDictionaryExtensions
     {
-        public static MultiValueDictionary<TKey, TValue> ToMultiValueDictionary<TKey, TValue>(this IEnumerable<TValue> values, Func<TValue, TKey> keySelector)
+        public static OrderedMultiValueDictionary<TKey, TValue> ToOrderedMultiValueDictionary<TKey, TValue>(this IEnumerable<TValue> values, Func<TValue, TKey> keySelector)
         {
-            var multiValueDictionary = new MultiValueDictionary<TKey, TValue>();
+            var orderedMultiValueDictionary = new OrderedMultiValueDictionary<TKey, TValue>();
             foreach (var entry in values)
-                multiValueDictionary.Add(keySelector.Invoke(entry), entry);
-            return multiValueDictionary;
+                orderedMultiValueDictionary.Add(keySelector.Invoke(entry), entry);
+            return orderedMultiValueDictionary;
         }
-        
-        public static MultiValueDictionary<TKey, TValue> ToMultiValueDictionary<T, TKey, TValue>(this IEnumerable<T> values, Func<T, TKey> keySelector, Func<T, TValue> valueSelector)
+
+        public static OrderedMultiValueDictionary<TKey, TValue> ToOrderedMultiValueDictionary<T, TKey, TValue>(this IEnumerable<T> values, Func<T, TKey> keySelector,
+            Func<T, TValue> valueSelector)
         {
-            var multiValueDictionary = new MultiValueDictionary<TKey, TValue>();
+            var orderedMultiValueDictionary = new OrderedMultiValueDictionary<TKey, TValue>();
             foreach (var entry in values)
             {
-                multiValueDictionary.Add(keySelector.Invoke(entry), valueSelector.Invoke(entry));
+                orderedMultiValueDictionary.Add(keySelector.Invoke(entry), valueSelector.Invoke(entry));
             }
 
-            return multiValueDictionary;
+            return orderedMultiValueDictionary;
         }
 
-        public static MultiValueDictionary<TKey, TSelectorValue> ToMultiValueDictionaryOfType<TKey, TValue, TSelectorValue>(this IEnumerable<TValue> values,
+        public static OrderedMultiValueDictionary<TKey, TSelectorValue> ToOrderedMultiValueDictionaryOfType<TKey, TValue, TSelectorValue>(this IEnumerable<TValue> values,
             Func<TValue, TKey> keySelector)
         {
-            var multiValueDictionary = new MultiValueDictionary<TKey, TSelectorValue>();
+            var orderedMultiValueDictionary = new OrderedMultiValueDictionary<TKey, TSelectorValue>();
             foreach (var entry in values)
             {
                 if (entry is TSelectorValue castedValue)
-                    multiValueDictionary.Add(keySelector.Invoke(entry), castedValue);
+                    orderedMultiValueDictionary.Add(keySelector.Invoke(entry), castedValue);
             }
 
-            return multiValueDictionary;
+            return orderedMultiValueDictionary;
         }
     }
 }
